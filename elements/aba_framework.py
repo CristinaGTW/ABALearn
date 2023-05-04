fa9c8e392418387ccfaffa4bee4af95ba29392d3
Cristina <ct519@ic.ac.uk>
2023-05-04 14:04:57 +0100

    Asp Translation

--- elements/aba_framework.py
+++ elements/aba_framework.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
 from elements.components import Rule, Example, Atom, Equality
+from copy import deepcopy
+from collections import defaultdict
 
 
 @dataclass
@@ -41,3 +43,155 @@ class ABAFramework:
             content += contrary[0].to_prolog_contrary(contrary[1]) + "\n"
 
         return content
+
+    def to_asp(self, filename: str):
+        asp_translation = 'p aba '
+        (all_instantiations, consts) = self.get_all_instantiations_and_consts()
+        assumptions = self.get_all_assumptions(consts)
+        contraries = self.get_all_contraries(consts)
+        language = []
+        for r in all_instantiations:
+            for atom in r:
+                language.append(atom)
+        for r in assumptions:
+            for atom in r:
+                language.append(atom)
+        for r in contraries:
+            for atom in r:
+                language.append(atom)
+        language = list(set(language))
+        language.sort()
+        asp_translation += f'{len(language)}' + '\n'
+        lang_map = {}
+        for i, lang in enumerate(language):
+            lang_map[lang] = i
+        for r in all_instantiations:
+            asp_translation += 'r '
+            for atom in r:
+                asp_translation += f'{lang_map[atom] + 1}' + ' '
+            asp_translation = asp_translation[:-1]
+            asp_translation += '\n'
+        for a in assumptions:
+            asp_translation += 'a '
+            for atom in a:
+                asp_translation += f'{lang_map[atom] + 1}' + ' '
+            asp_translation = asp_translation[:-1]
+            asp_translation += '\n'
+        for c in contraries:
+            asp_translation += 'c '
+            for atom in c:
+                asp_translation += f'{lang_map[atom] + 1}' + ' '
+            asp_translation = asp_translation[:-1]
+            asp_translation += '\n'
+
+        f = open(filename, "w")
+        f.write(asp_translation)
+        f.close()
+
+    def get_all_assumptions(self, consts):
+        ret = []
+        for a in self.assumptions:
+            result = []
+            self._get_all_groundings([a], 0, 0, {}, {0: []}, consts, result)
+            ret += result
+        return ret
+
+    def get_all_contraries(self, consts):
+        ret = []
+        for (a, c_a) in self.contraries:
+            result = []
+            self._get_all_groundings([a, c_a], 0, 0, {}, {
+                                     0: [], 1: []}, consts, result)
+            ret += result
+        return ret
+
+    def get_all_instantiations_and_consts(self):
+        consts = []
+        var_val_map = defaultdict(dict)
+        for r in self.background_knowledge:
+            head = r.head
+            for arg in head.arguments:
+                if not arg[0].isupper():
+                    consts.append(arg)
+            for b in r.body:
+                if isinstance(b, Atom):
+                    for arg in b.arguments:
+                        if not arg[0].isupper():
+                            consts.append(arg)
+                else:
+                    if not b.var_1[0].isupper():
+                        consts.append(b.var_1)
+                    else:
+                        var_val_map[r.rule_id][b.var_1] = b.var_2
+                    if not b.var_2[0].isupper():
+                        consts.append(b.var_2)
+
+        rules = deepcopy(self.background_knowledge)
+        for i, r in enumerate(rules):
+            no_change = False
+            while not no_change:
+                no_change = True
+                for j, b in enumerate(r.body):
+                    if isinstance(b, Atom):
+                        for k, a in enumerate(b.arguments):
+                            if a in var_val_map[r.rule_id]:
+                                rules[i].body[j].arguments[k] = var_val_map[r.rule_id][a]
+                                no_change = False
+                    if isinstance(b, Equality):
+                        if b.var_2 in var_val_map[r.rule_id] and var_val_map[r.rule_id][b.var_2] != b.var_1:
+                            var_val_map[r.rule_id][b.var_1] = var_val_map[r.rule_id][b.var_2]
+                            no_change = False
+            for j, a in enumerate(r.head.arguments):
+                if a in var_val_map:
+                    rules[i].head.arguments[j] = var_val_map[r.rule_id][a]
+        consts = set(consts)
+        ret = []
+        for r in rules:
+            atoms = [r.head] + r.get_atoms()
+            result = []
+            curr_atoms_args = {}
+            for i in range(len(atoms)):
+                curr_atoms_args[i] = []
+            self._get_all_groundings(
+                atoms, 0, 0, var_val_map[r.rule_id], curr_atoms_args, consts, result)
+            ret += result
+        return (ret, consts)
+
+    def _get_all_groundings(self, atoms: list[Atom], idx: int, arg_idx: int, var_val_map: dict[str, str], curr_atoms_args: dict[int, list[str]], consts: list[str], result: list[list[str]]):
+        while idx < len(atoms):
+            while arg_idx < len(atoms[idx].arguments):
+                arg = atoms[idx].arguments[arg_idx]
+                prev_args = deepcopy(curr_atoms_args[idx][:arg_idx])
+                if arg in var_val_map:
+                    curr_atoms_args[idx] = prev_args + [var_val_map[arg]]
+                    self._replace_all(curr_atoms_args, arg, var_val_map[arg])
+                else:
+                    if arg[0].isupper():
+                        for c in consts:
+                            var_val_map[arg] = c
+                            curr_atoms_args[idx] = prev_args + [c]
+                            curr_atoms_args = self._replace_all(
+                                curr_atoms_args, arg, c)
+                            self._get_all_groundings(
+                                atoms, idx, arg_idx+1, var_val_map, curr_atoms_args, consts, result)
+                        arg_idx += 1
+                    else:
+                        curr_atoms_args[idx] = prev_args+[arg]
+                arg_idx += 1
+            arg_idx = 0
+            idx += 1
+        instantiation = []
+        for i, a in enumerate(atoms):
+            atom_str = a.predicate + '('
+            for arg in curr_atoms_args[i]:
+                atom_str += arg + ','
+            atom_str = atom_str[:-1]
+            atom_str += ')'
+            instantiation += [atom_str]
+        if instantiation not in result:
+            result.append(instantiation)
+
+    def _replace_all(self, d: dict[int, list[str]], a: str, b: str):
+        for k in d:
+            d[k] = [b if x == a else x for x in d[k]]
+        return d
---
+++ test/test_aspforaba.py
@@ -0,0 +1,36 @@
+from elements.aba_framework import ABAFramework
+from elements.components import Rule, Atom
+
+
+def test_get_all_instantiations_and_consts():
+    rule_1 = Rule.parse_rule("r1:p(A,B)<-q(A,C),r(C,B),A=B,C=3")
+    rule_2 = Rule.parse_rule("r2:p(A,B)<-A=1,B=2")
+    rule_3 = Rule.parse_rule("r3:p(A,B)<-A=3")
+    background_knowledge = [rule_1, rule_2, rule_3]
+    aba_framework = ABAFramework(
+        background_knowledge, [], [], [], [], {}, {}, {})
+    (asp,_) = aba_framework.get_all_instantiations_and_consts()
+    asp.sort()
+    assert asp == [['p(1,1)', 'q(1,3)', 'r(3,1)'], ['p(1,2)'], ['p(2,2)', 'q(2,3)', 'r(3,2)'], [
+        'p(3,1)'], ['p(3,2)'], ['p(3,3)'], ['p(3,3)', 'q(3,3)', 'r(3,3)']]
+
+
+def test_get_all_groundings():
+    aba_framework = ABAFramework([], [], [], [], [], {}, {}, {})
+    atom = [Atom("p", ["A", "2"])]
+    result = []
+    aba_framework._get_all_groundings(
+        atom, 0, 0,{}, {0:[]},["1", "2", "3"], result)
+    assert result == [['p(1,2)'], ['p(2,2)'], ['p(3,2)']]
+
+def test_asp_file():
+    rule_1 = Rule.parse_rule("r1:p(A,B)<-q(A,C),r(C,B),A=B,C=3")
+    rule_2 = Rule.parse_rule("r2:p(A,B)<-A=1,B=2")
+    rule_3 = Rule.parse_rule("r3:p(A,B)<-A=3")
+    a_1 = Atom.parse_atom("alpha(A)")
+    c_a_1 = Atom.parse_atom("c_alpha(A)")
+    background_knowledge = [rule_1, rule_2, rule_3]
+    aba_framework = ABAFramework(
+        background_knowledge, [], [], [a_1], [(a_1,c_a_1)], {}, {}, {})
+    aba_framework.to_asp("test_asp.pl")
+    assert 'p aba 18\nr 9 14 17\nr 12 15 18\nr 7 13 16\nr 8\nr 11\nr 12\nr 10\na 2\na 3\na 1\nc 2 5\nc 3 6\nc 1 4\n' == open("test_asp.pl").read()
